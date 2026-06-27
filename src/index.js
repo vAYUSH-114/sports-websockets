416aa9036da9def55ae953916fb7ad645e8a0d04
Your Name <you@example.com>
2026-06-27 12:09:55 +0530

    implemenetd matches post

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from 'express';
 const app = express();
 const port = 8000;
+import { matchRouter } from './routes/matches.js'
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('helo');
 })
+
+app.use("/matches", matchRouter)
 
 app.listen(port, () => (console.log(`server starts running on port: 8000`)))
