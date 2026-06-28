e64a43b94ae94e1a36c64e1d4a91106323cf4cd0
Your Name <you@example.com>
2026-06-28 15:32:52 +0530

    implemented arcjet

--- src/ws/server.js
+++ src/ws/server.js
@@ -1,4 +1,5 @@
 import { WebSocket, WebSocketServer } from "ws";
+import { wsArcjet } from "../arcjet.js";
 
 //send the json to those servber which is open
 function sendJson(socket, payload) {
@@ -20,6 +21,11 @@ function broadcast(wss, payload) {
 }
 
 export function attachWebSocketServer(server) {
+
+
+
+
+
     // we are using the same server/ http server for requesting for websocket
     const wss = new WebSocketServer({
         server,
@@ -28,7 +34,30 @@ export function attachWebSocketServer(server) {
     })
 
 
-    wss.on('connection', (socket) => {
+    wss.on('connection', async (socket, req) => {
+
+        if (wsArcjet) {
+            try {
+                const decision = await wsArcjet.protect(req);
+                if (decision.isDenied()) {
+                    const code = decision.reason.isRateLimit() ? 1013 : 1008; //1013 rate limit reached try again later and 1008 a bot is dectcted
+                    const reason = decision.reason.isRateLimit() ? 'Rate limit reached' : 'acces denied';
+
+                    socket.close(code, reason);
+                    return;
+                }
+            } catch (e) {
+                console.error(`ws connection failure`, e)
+                socket.close(1011, 'server security error');
+                return;
+            }
+
+
+
+        }
+
+
+
         socket.isAlive = true;
         socket.on('pong', () => { socket.isAlive = true; });
 
